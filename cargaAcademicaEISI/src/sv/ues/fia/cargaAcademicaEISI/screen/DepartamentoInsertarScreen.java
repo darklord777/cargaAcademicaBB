55c957c2b61d68fb41ebb0ca7ad097ac9bfc5d03
mario <mariosanchezsv87@gmail.com>
2013-05-23 18:43:47 -0600

    drkl

--- cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/DepartamentoInsertarScreen.java
+++ cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/DepartamentoInsertarScreen.java
@@ -1,14 +1,13 @@
 package sv.ues.fia.cargaAcademicaEISI.screen;
 
-import sv.ues.fia.cargaAcademicaEISI.Ciclo;
-import sv.ues.fia.cargaAcademicaEISI.ControlBD;
-import sv.ues.fia.cargaAcademicaEISI.Departamento;
 import net.rim.device.api.ui.Field;
 import net.rim.device.api.ui.FieldChangeListener;
 import net.rim.device.api.ui.component.ButtonField;
 import net.rim.device.api.ui.component.EditField;
 import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.MainScreen;
+import sv.ues.fia.cargaAcademicaEISI.ControlBD;
+import sv.ues.fia.cargaAcademicaEISI.Departamento;
 
 public class DepartamentoInsertarScreen extends MainScreen implements
 		FieldChangeListener {
@@ -52,16 +51,13 @@ public class DepartamentoInsertarScreen extends MainScreen implements
 
 				if (a) {
 					lb1.setText("Insercion realizada con exito.");
-					anio.setText("");
-					numero.setText("");
-					fechaini.setText("");
-					fechafin.setText("");
-					anio.setFocus();
+					idepto.setText("");
+					nomdepto.setText("");
+					idepto.setFocus();
 				}
 
 				else
 					lb1.setText("No se pudo realizar la insercion.");
-
 			}
 
 		}
