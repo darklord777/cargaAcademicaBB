22933af9d61ffd3c02736b78f2f260405323aa6e
Michael <miguel.a1906@gmail.com>
2013-05-24 12:56:59 +0200

    UP

--- cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadActAcadEliminarScreen.java
+++ cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadActAcadEliminarScreen.java
@@ -1,15 +1,50 @@
 package sv.ues.fia.cargaAcademicaEISI.screen;
 
+import sv.ues.fia.cargaAcademicaEISI.ControlBD;
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.FieldChangeListener;
+import net.rim.device.api.ui.component.ButtonField;
+import net.rim.device.api.ui.component.EditField;
+import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.MainScreen;
 
-public class ModalidadActAcadEliminarScreen extends MainScreen {
+public class ModalidadActAcadEliminarScreen extends MainScreen implements FieldChangeListener {
 
+	private EditField idModAA;
+	private ButtonField bt1;
+	private LabelField lb1;
+	public ControlBD bdh;
+	
 	/**
 	 * 
 	 */
 	public ModalidadActAcadEliminarScreen() {
 		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
 		// TODO Auto-generated constructor stub
+		setTitle("Eliminar Modalidad Actividad Academica"); 
+		 bdh = new ControlBD();
+		 idModAA = new EditField("Codigo de Modalidad:  ","",6,EditField.FILTER_DEFAULT);
+		 bt1 = new ButtonField("Eliminar Registro",ButtonField.CONSUME_CLICK);
+		 bt1.setChangeListener(this);
+		 lb1 = new LabelField("");
+		 add(idModAA);
+		 add(bt1);
+		 add(lb1);
+	}
+
+	public void fieldChanged(Field field, int context) {
+		// TODO Auto-generated method stub
+		boolean m;
+		if(field.equals(bt1))
+		{
+			bdh.Abrir();
+			m=bdh.eliminarLocal(idModAA.getText());
+			bdh.Cerrar();
+		if(m)
+			lb1.setText("Se elimino el local.");
+		else
+			lb1.setText("Error en eliminacion.");
+		}
 	}
 
 }
--- cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadCursoEliminarScreen.java
+++ cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadCursoEliminarScreen.java
@@ -1,15 +1,48 @@
 package sv.ues.fia.cargaAcademicaEISI.screen;
 
+import sv.ues.fia.cargaAcademicaEISI.ControlBD;
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.FieldChangeListener;
+import net.rim.device.api.ui.component.ButtonField;
+import net.rim.device.api.ui.component.EditField;
+import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.MainScreen;
 
-public class ModalidadCursoEliminarScreen extends MainScreen {
+public class ModalidadCursoEliminarScreen extends MainScreen implements FieldChangeListener {
 
+	private EditField idModC;
+	private ButtonField bt1;
+	private LabelField lb1;
+	public ControlBD bdh;
 	/**
 	 * 
 	 */
 	public ModalidadCursoEliminarScreen() {
 		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
 		// TODO Auto-generated constructor stub
+		setTitle("Eliminar Modalidad Curso"); 
+		 bdh = new ControlBD();
+		 idModC = new EditField("Codigo de Modalidad:  ","",6,EditField.FILTER_DEFAULT);
+		 bt1 = new ButtonField("Eliminar Registro",ButtonField.CONSUME_CLICK);
+		 bt1.setChangeListener(this);
+		 lb1 = new LabelField("");
+		 add(idModC);
+		 add(bt1);
+		 add(lb1);
+	}
+	public void fieldChanged(Field field, int context) {
+		// TODO Auto-generated method stub
+		boolean m;
+		if(field.equals(bt1))
+		{
+			bdh.Abrir();
+			m=bdh.eliminarLocal(idModC.getText());
+			bdh.Cerrar();
+		if(m)
+			lb1.setText("Se elimino el local.");
+		else
+			lb1.setText("Error en eliminacion.");
+		}
 	}
 
 }
