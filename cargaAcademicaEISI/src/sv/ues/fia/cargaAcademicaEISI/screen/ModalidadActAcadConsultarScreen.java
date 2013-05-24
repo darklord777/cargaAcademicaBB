2ea3c15b1a7eca7d5a192356330e7e0b61c7048e
Michael <miguel.a1906@gmail.com>
2013-05-24 12:29:25 +0200

    UP

--- cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadActAcadConsultarScreen.java
+++ cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadActAcadConsultarScreen.java
@@ -1,8 +1,22 @@
 package sv.ues.fia.cargaAcademicaEISI.screen;
 
+import sv.ues.fia.cargaAcademicaEISI.ControlBD;
+import sv.ues.fia.cargaAcademicaEISI.Modalidad_ActAcad;
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.FieldChangeListener;
+import net.rim.device.api.ui.component.ButtonField;
+import net.rim.device.api.ui.component.EditField;
+import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.MainScreen;
 
-public class ModalidadActAcadConsultarScreen extends MainScreen {
+public class ModalidadActAcadConsultarScreen extends MainScreen implements FieldChangeListener {
+
+	private EditField idModAA;
+	private EditField nombreMAA;
+	private EditField DescH;
+	private ButtonField bt1;
+	public ControlBD bdh;
+	private LabelField lb1;
 
 	/**
 	 * 
@@ -10,6 +24,43 @@ public class ModalidadActAcadConsultarScreen extends MainScreen {
 	public ModalidadActAcadConsultarScreen() {
 		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
 		// TODO Auto-generated constructor stub
+		setTitle("Consultar Modalidad Actividad Academica");
+		bdh = new ControlBD();
+		idModAA = new EditField("Codigo Modalidad:","",6,EditField.FILTER_DEFAULT);
+		nombreMAA = new EditField("Nombre:","",20,EditField.FILTER_DEFAULT);
+		DescH = new EditField("Descuento Horas:","",5,EditField.FILTER_DEFAULT);
+		
+		bt1 = new ButtonField("Consultar Modalidad Act Acad",ButtonField.CONSUME_CLICK);
+		bt1.setChangeListener(this);
+		lb1 = new LabelField("");
+		nombreMAA.setEditable(false);
+		DescH.setEditable(false);
+		add(idModAA);
+		add(bt1);
+		add(nombreMAA);
+		add(DescH);
+		add(lb1);
+	}
+
+	public void fieldChanged(Field field, int context) {
+		// TODO Auto-generated method stub
+		if(field.equals(bt1))
+		{
+		Modalidad_ActAcad al = new Modalidad_ActAcad();
+		bdh.Abrir();
+		al = bdh.ConsultarModActA(idModAA.getText());
+		bdh.Cerrar();
+		if(al!=null)
+		{
+		nombreMAA.setText(al.getNombreModalAA());
+		DescH.setText(al.getDesHrsMAA());
+		lb1.setText("");
+		}
+		else
+		{
+		lb1.setText("No se encuentra la modalidad.");
+		}
+		}
 	}
 
 }
--- cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadCursoConsultarScreen.java
+++ cargaAcademicaEISI/src/sv/ues/fia/cargaAcademicaEISI/screen/ModalidadCursoConsultarScreen.java
@@ -1,15 +1,66 @@
 package sv.ues.fia.cargaAcademicaEISI.screen;
 
+import sv.ues.fia.cargaAcademicaEISI.ControlBD;
+import sv.ues.fia.cargaAcademicaEISI.Modalidad_Curso;
+import net.rim.device.api.ui.Field;
+import net.rim.device.api.ui.FieldChangeListener;
+import net.rim.device.api.ui.component.ButtonField;
+import net.rim.device.api.ui.component.EditField;
+import net.rim.device.api.ui.component.LabelField;
 import net.rim.device.api.ui.container.MainScreen;
 
-public class ModalidadCursoConsultarScreen extends MainScreen {
+public class ModalidadCursoConsultarScreen extends MainScreen implements FieldChangeListener {
 
+	private EditField idModC;
+	private EditField nombreMC;
+	private EditField DescH;
+	private ButtonField bt1;
+	public ControlBD bdh;
+	private LabelField lb1;
+	
 	/**
 	 * 
 	 */
 	public ModalidadCursoConsultarScreen() {
 		super(MainScreen.VERTICAL_SCROLL | MainScreen.VERTICAL_SCROLLBAR);
 		// TODO Auto-generated constructor stub
+		setTitle("Consultar Modalidad Curso");
+		bdh = new ControlBD();
+		idModC = new EditField("Codigo Modalidad:","",6,EditField.FILTER_DEFAULT);
+		nombreMC = new EditField("Nombre:","",20,EditField.FILTER_DEFAULT);
+		DescH = new EditField("Descuento Horas:","",5,EditField.FILTER_DEFAULT);
+		
+		bt1 = new ButtonField("Consultar Modalidad",ButtonField.CONSUME_CLICK);
+		bt1.setChangeListener(this);
+		lb1 = new LabelField("");
+		nombreMC.setEditable(false);
+		DescH.setEditable(false);
+		add(idModC);
+		add(bt1);
+		add(nombreMC);
+		add(DescH);
+		add(lb1);
+	}
+
+	public void fieldChanged(Field field, int context) {
+		// TODO Auto-generated method stub
+		if(field.equals(bt1))
+		{
+		Modalidad_Curso al = new Modalidad_Curso();
+		bdh.Abrir();
+		al = bdh.ConsultarModCurso(idModC.getText());
+		bdh.Cerrar();
+		if(al!=null)
+		{
+		nombreMC.setText(al.getNombreModal());
+		DescH.setText(al.getDescHoras());
+		lb1.setText("");
+		}
+		else
+		{
+		lb1.setText("No se encuentra la modalidad.");
+		}
+		}
 	}
 
 }
